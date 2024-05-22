b545aca3686e50d7ede804713c1853e7ecd1c6ec
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-22 15:31:51 +0300

    Add: TaskCreateView to views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
 from tasks.models import Task
 
@@ -7,3 +8,10 @@ class TaskListView(generic.ListView):
     model = Task
     context_object_name = "task_list"
     template_name = "tasks/task_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
