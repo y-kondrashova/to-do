d848afea2685fc7be366601ff3352ffb4ab380f5
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-22 15:35:09 +0300

    Add: path to urls.py in tasks app
    Url to:
    task-list
    tag-list
    tag-create
    task-create

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-urlpatterns = [
+from tasks.views import (
+    TaskListView,
+    TaskCreateView,
+    TagListView,
+    TagCreateView
+)
 
+urlpatterns = [
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tasks/cteate/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 app_name = "tasks"
