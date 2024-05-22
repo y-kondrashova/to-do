d3e282f1f400f5469eb07aa4dba0d24a18aae7cc
Yevheniia Kondrashova <evgeniia.kondrashova@gmail.com>
2024-05-22 15:55:13 +0300

    Add: TagUpdateView to project

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,13 +4,15 @@ from tasks.views import (
     TaskListView,
     TaskCreateView,
     TagListView,
-    TagCreateView
+    TagCreateView,
+    TagUpdateView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag-update"),
     path("tasks/cteate/", TaskCreateView.as_view(), name="task-create"),
 ]
 
--- tasks/views.py
+++ tasks/views.py
@@ -26,3 +26,9 @@ class TagCreateView(generic.CreateView):
     model = Tag
     fields = "__all__"
     success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
